68ef31f2ef689f8cb54d2d4f71d3d60828c78e1f
narinekarakavorian <narinek0418@gmail.com>
2024-07-29 23:27:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Fashioner Designer", "Artistic Director", "Stylist"],
+        strings: ["Fashion Designer", "Artistic Director", "Stylist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Fashioner Designer", "Artistic Director", "Stylist"],
+        strings: ["Fashion Designer", "Artistic Director", "Stylist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
